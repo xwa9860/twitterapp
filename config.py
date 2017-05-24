e457a6a734f243c847834bad372d1a030c40bd37
Xin Wang <imwangxin@gmail.com>
2017-05-24 11:42:04 -0700

    set up auto env

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = 'very-secure' 
+    SECRET_KEY = 'very-secure'
 
 
 class ProductionConfig(Config):
--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 from app import app
+import os
 
 if __name__ == '__main__':
+    print(os.environ.get('APP_SETTINGS'))
     app.run()
