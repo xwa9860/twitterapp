4641cd42c8d342c5d084eb9a2f08e5f5a770397e
Xin Wang <imwangxin@gmail.com>
2017-05-24 12:36:15 -0700

    added database

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
 import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-from app.views import view
+from app.models import model
+#from app.views import view
 
