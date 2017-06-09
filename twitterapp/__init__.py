86f07e8b32e2f5dbe74ff6c41e53f472e0408596
Xin Wang <imwangxin@gmail.com>
2017-06-09 09:38:06 -0700

    added print app settings variable to check config on heroku

--- twitterapp/__init__.py
+++ twitterapp/__init__.py
@@ -8,10 +8,10 @@ from worker import conn
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+print(os.environ['APP_SETTINGS'])
 
 twitter_db = SQLAlchemy(app)
 q = Queue(connection=conn)
 
 from twitterapp.views import view
 from twitterapp.models import model
-
